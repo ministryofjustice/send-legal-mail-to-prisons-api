3790f4832b56754c5c76374b41f29f7f09412a53
petergphillips <peter@greenthistle.co.uk>
2021-11-04 15:59:43 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.12"
+  kotlin("plugin.spring") version "1.5.31"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "16"
+    }
+  }
+}
