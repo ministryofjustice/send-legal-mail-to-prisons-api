f5f93f82cbf307cf7d6412189228865b9affef91
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-11-16 16:36:28 +0000

    Revert "403 Debug different timestamps"
    This reverts commit 29eaee697337811177a90febdbc338ee47512c77.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/integration/BarcodeEventRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/integration/BarcodeEventRepositoryTest.kt
@@ -2,24 +2,17 @@ package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.integration
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
 import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.barcode.Barcode
 import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.barcode.BarcodeEvent
 
 class BarcodeEventRepositoryTest : IntegrationTest() {
 
-  private val log = LoggerFactory.getLogger(this::class.java)
-
   @Test
   fun `can create and retrieve a barcode event`() {
     val barcode = barcodeRepository.save(Barcode("SOME_BARCODE"))
-    val event = BarcodeEvent(barcode = barcode, userId = "SOME_USER")
-    log.info(">>>>>> event has datetime=${event.dateTime}")
-    val barcodeEvent = barcodeEventRepository.save(event)
-    log.info(">>>>>> barcode event has datetime=${barcodeEvent.dateTime}")
+    val barcodeEvent = barcodeEventRepository.save(BarcodeEvent(barcode = barcode, userId = "SOME_USER"))
 
     val savedBarcodeEvent = barcodeEventRepository.findById(barcodeEvent.id).orElseThrow()
-    log.info(">>>>>> saved barcode event has datetime=${savedBarcodeEvent.dateTime}")
 
     assertThat(savedBarcodeEvent).isEqualTo(barcodeEvent)
   }
