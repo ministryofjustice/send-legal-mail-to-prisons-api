528f7908f13f8250627724399fad8cfa1775796a
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-11-24 12:58:08 +0000

    SLM-12 generate JWT (#15)
    * SLM-12 - add instructions for how to generate the public and private keys
    
    * SLM-12 - add a service for creating JWTs
    
    * SLM-12 - tidy

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/config/JwtConfig.kt
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import java.time.Duration
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "app.jwt")
+data class JwtConfig(
+  val privateKey: String,
+  val publicKey: String,
+  val expiry: Duration,
+)
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/security/JwtService.kt
@@ -0,0 +1,65 @@
+package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.security
+
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import mu.KotlinLogging
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.config.JwtConfig
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.temporal.ChronoUnit
+import java.util.Base64
+import java.util.Date
+import java.util.UUID
+
+private val log = KotlinLogging.logger {}
+
+@Service
+class JwtService(jwtConfig: JwtConfig) {
+
+  private val privateKey: PrivateKey = readPrivateKey(jwtConfig.privateKey)
+  private val publicKey: PublicKey = readPublicKey(jwtConfig.publicKey)
+  private val expiry: Duration = jwtConfig.expiry
+
+  private fun readPrivateKey(privateKeyString: String): PrivateKey =
+    PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyString))
+      .let { KeyFactory.getInstance("RSA").generatePrivate(it) }
+
+  private fun readPublicKey(publicKeyString: String): PublicKey =
+    X509EncodedKeySpec(Base64.getDecoder().decode(publicKeyString))
+      .let { KeyFactory.getInstance("RSA").generatePublic(it) }
+
+  fun generateToken(email: String): String =
+    Jwts.builder()
+      .setId(UUID.randomUUID().toString())
+      .setSubject(email)
+      .setExpiration(
+        Date.from(
+          LocalDateTime.now().plus(expiry.toMillis(), ChronoUnit.MILLIS).toInstant(zoneOffset())
+        )
+      )
+      .signWith(SignatureAlgorithm.RS256, privateKey)
+      .compact()
+
+  private fun zoneOffset() = ZoneId.systemDefault().rules.getOffset(Instant.now())
+
+  fun validateToken(jwt: String): Boolean =
+    runCatching {
+      Jwts.parser().setSigningKey(publicKey).parseClaimsJws(jwt)
+    }.onFailure {
+      if ((it is ExpiredJwtException).not()) {
+        log.warn("Found an invalid JWT: jwt", it)
+      }
+    }.isSuccess
+
+  fun subject(jwt: String): String =
+    Jwts.parser().setSigningKey(publicKey).parseClaimsJws(jwt).body.subject
+}
