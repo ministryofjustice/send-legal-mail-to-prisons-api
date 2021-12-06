25d1356454bf63994184d29e9b211cce44594a1a
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-12-06 16:25:58 +0000

    SLM-21 Correct versions for Kotlin / JDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.13"
   id("org.unbroken-dome.test-sets") version "4.0.0"
-  kotlin("plugin.spring") version "1.6.0"
-  kotlin("plugin.jpa") version "1.6.0"
+  kotlin("plugin.spring") version "1.5.31"
+  kotlin("plugin.jpa") version "1.5.31"
 }
 
 testSets {
