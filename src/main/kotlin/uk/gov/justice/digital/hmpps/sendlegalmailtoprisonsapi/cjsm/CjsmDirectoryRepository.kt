5f6796139edad2637b0a0dfe97d43beee9fd64d1
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-12-15 16:46:51 +0000

    SLM-53 Add a service to save CJSM directory csv lines to the DB

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/cjsm/CjsmDirectoryRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/cjsm/CjsmDirectoryRepository.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.cjsm
 
+import org.hibernate.Hibernate
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import javax.persistence.Entity
@@ -26,4 +27,18 @@ data class CjsmDirectoryEntry(
   val organisation: String,
   val townCity: String,
   val businessType: String,
-)
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+    other as CjsmDirectoryEntry
+
+    return secureEmail == other.secureEmail
+  }
+
+  override fun hashCode(): Int = secureEmail.hashCode()
+
+  override fun toString(): String {
+    return "CjsmDirectoryEntry(secureEmail=$secureEmail, firstName='$firstName', lastName=$lastName, organisation=$organisation, townCity=$townCity, businessType=$businessType)"
+  }
+}
