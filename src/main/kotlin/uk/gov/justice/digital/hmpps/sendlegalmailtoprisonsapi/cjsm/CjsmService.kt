848f64ee5cbbd1a872b3c73013b67a3cc867f0ea
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-12-16 11:57:38 +0000

    SLM-53 Create cjsm directory table and popualte from CSV (#39)
    * SLM-53 Add new table for CJSM directory
    
    * SLM-53 Remove old repository tests - don't really need them anymore
    
    * SLM-53 Add a service to save CJSM directory csv lines to the DB
    
    * SLM-53 Remove the repo test - it's covered elsewhere

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/cjsm/CjsmService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/cjsm/CjsmService.kt
@@ -2,14 +2,48 @@ package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.cjsm
 
 import com.amazonaws.services.s3.AmazonS3
 import org.springframework.stereotype.Service
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 
 @Service
-class CjsmService(private val amazonS3Client: AmazonS3, private val s3Config: S3Config) {
+class CjsmService(
+  private val amazonS3: AmazonS3,
+  private val s3Config: S3Config,
+  private val cjsmDirectoryRepository: CjsmDirectoryRepository
+) {
 
   fun readCjsmOrgs(): String =
     try {
-      amazonS3Client.getObjectAsString(s3Config.bucketName, s3Config.cjsmOrgsCsv)
+      amazonS3.getObjectAsString(s3Config.bucketName, s3Config.cjsmOrgsCsv)
     } catch (ex: Exception) {
       "An error occurred attempting to read ${s3Config.cjsmOrgsCsv} from S3 bucket ${s3Config.bucketName}: ${ex.message}"
     }
+
+  fun saveCjsmDirectoryCsv() =
+    amazonS3.getObject(s3Config.bucketName, s3Config.cjsmOrgsCsv)
+      .objectContent
+      .let { streamCjsmDirectoryCsv(it) }
+
+  fun streamCjsmDirectoryCsv(inputStream: InputStream) =
+    inputStream.let { BufferedReader(InputStreamReader(BufferedInputStream(it))) }
+      .useLines { lines ->
+        lines.mapNotNull { line -> fromCjsmDirectoryCsvLine(line) }
+          .forEach { cjsmDirectoryEntry -> cjsmDirectoryRepository.save(cjsmDirectoryEntry) }
+      }
+
+  private fun fromCjsmDirectoryCsvLine(csvLine: String): CjsmDirectoryEntry? =
+    csvLine.split(",")
+      .takeIf { !it.contains("Secure Email") }
+      ?.let { propertyList ->
+        CjsmDirectoryEntry(
+          firstName = propertyList[0],
+          lastName = propertyList[1],
+          organisation = propertyList[3],
+          secureEmail = propertyList[4],
+          townCity = propertyList[7],
+          businessType = propertyList[12]
+        )
+      }
 }
