9c38a3c3b9c1f87e30248eba9843a2e2c1c3c8f6
Nathan Russell <nathan.russell@digital.justice.gov.uk>
2022-01-27 16:28:28 +0000

    SLM-108: Basic initial API with swagger spec

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/contact/ContactResource.kt
@@ -0,0 +1,104 @@
+package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.contact
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.config.ErrorResponse
+import java.time.LocalDate
+
+@RestController
+@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+class ContactResource(private val contactService: ContactService) {
+
+  @PostMapping(value = ["/contact"])
+  @ResponseBody
+  @ResponseStatus(HttpStatus.CREATED)
+  @PreAuthorize("hasRole('ROLE_SLM_CREATE_BARCODE')")
+  @Operation(
+    summary = "Creates a new Contact for the logged in user",
+    security = [SecurityRequirement(name = "ROLE_SLM_CREATE_BARCODE")]
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "201",
+        description = "Contact created",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ContactResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid magic link token",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "409",
+        description = "Conflict, the specified new contact already exists for this user",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ]
+  )
+  // TODO exception handler in order to return a 409 if createContact throws xxxException
+  fun createContact(@RequestBody createContactRequest: CreateContactRequest, authentication: Authentication): ContactResponse =
+    contactService.createContact(authentication.name, createContactRequest).let {
+      return ContactResponse(
+        id = it.id!!,
+        prisonerName = it.name,
+        prisonId = it.prisonCode,
+        dob = it.dob,
+        prisonNumber = it.prisonNumber
+      )
+    }
+}
+
+// TODO - apply validation that either dob or prisonNumber are present
+data class CreateContactRequest(
+  @Schema(description = "The name of the new contact", example = "John Doe", required = true)
+  val prisonerName: String,
+
+  @Schema(description = "The ID of the prison location of the new contact", example = "BXI", required = true)
+  val prisonId: String,
+
+  @Schema(description = "The date of birth of the new contact if known", example = "23/04/1965", required = false)
+  @JsonFormat(pattern = "dd/MM/yyyy")
+  val dob: LocalDate?,
+
+  @Schema(description = "The prison number of the new contact if known", example = "A1234BC", required = false)
+  val prisonNumber: String?,
+)
+
+data class ContactResponse(
+  @Schema(description = "The ID of contact", example = "1", required = true)
+  val id: Long,
+
+  @Schema(description = "The name of the contact", example = "John Doe", required = true)
+  val prisonerName: String,
+
+  @Schema(description = "The ID of the prison location of the contact", example = "BXI", required = true)
+  val prisonId: String,
+
+  @Schema(description = "The date of birth of the contact if known", example = "23/04/1965", required = false)
+  @JsonFormat(pattern = "dd/MM/yyyy")
+  val dob: LocalDate?,
+
+  @Schema(description = "The prison number of the contact if known", example = "A1234BC", required = false)
+  val prisonNumber: String?
+)
