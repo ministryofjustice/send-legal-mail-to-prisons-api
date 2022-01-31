46112868aa1f03d04dbc8e3666281adc73b468d8
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-01-31 11:52:17 +0000

    SLM-108: Request body validation (#59)

---
+++ src/testIntegration/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/contact/ContactResourceTest.kt
@@ -0,0 +1,133 @@
+package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.contact
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.IntegrationTest
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.config.AuthenticationError
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.config.MalformedRequest
+
+class ContactResourceTest : IntegrationTest() {
+
+  private companion object {
+    val JOHN_SMITH = CreateContactRequest(
+      prisonerName = "John Smith",
+      prisonId = "BXI",
+      prisonNumber = "A1234BC"
+    )
+  }
+
+  @Nested
+  inner class CreateContact {
+    @Test
+    fun `unauthorised without a valid auth token`() {
+      webTestClient.post()
+        .uri("/contact")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    @Test
+    fun `forbidden without a valid role`() {
+      webTestClient.post()
+        .uri("/contact")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(user = "AUSER_GEN"))
+        .bodyValue(JOHN_SMITH)
+        .exchange()
+        .expectStatus().isForbidden
+        .expectBody().jsonPath("$.errorCode.code").isEqualTo(AuthenticationError.code)
+    }
+
+    @Test
+    fun `bad request given empty body`() {
+      webTestClient.post()
+        .uri("/contact")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setCreateBarcodeAuthorisation())
+        .bodyValue("{}")
+        .exchange()
+        .expectStatus().isBadRequest
+        .expectBody().jsonPath("$.errorCode.code").isEqualTo(MalformedRequest.code)
+    }
+
+    @Test
+    fun `bad request given invalid dob format`() {
+      webTestClient.post()
+        .uri("/contact")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setCreateBarcodeAuthorisation())
+        .bodyValue(
+          """{ 
+            "prisonerName": "John Smith",
+            "prisonId": "BXI",
+            "dob": "17-08-1971"
+          }"""
+        )
+        .exchange()
+        .expectStatus().isBadRequest
+        .expectBody().jsonPath("$.errorCode.code").isEqualTo(MalformedRequest.code)
+    }
+
+    @Test
+    fun `bad request given neither prison number or dob`() {
+      webTestClient.post()
+        .uri("/contact")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setCreateBarcodeAuthorisation())
+        .bodyValue(
+          """{ 
+            "prisonerName": "John Smith",
+            "prisonId": "BXI"
+          }"""
+        )
+        .exchange()
+        .expectStatus().isBadRequest
+        .expectBody().jsonPath("$.errorCode.code").isEqualTo(MalformedRequest.code)
+    }
+
+    @Test
+    fun `bad request given invalid format prisonNumber`() {
+      webTestClient.post()
+        .uri("/contact")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setCreateBarcodeAuthorisation())
+        .bodyValue(
+          """{ 
+            "prisonerName": "John Smith",
+            "prisonId": "BXI",
+            "prisonNumber": "not a valid value"
+          }"""
+        )
+        .exchange()
+        .expectStatus().isBadRequest
+        .expectBody().jsonPath("$.errorCode.code").isEqualTo(MalformedRequest.code)
+    }
+
+    @Test
+    fun `bad request given invalid format prisonId`() {
+      webTestClient.post()
+        .uri("/contact")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setCreateBarcodeAuthorisation())
+        .bodyValue(
+          """{ 
+            "prisonerName": "John Smith",
+            "prisonId": "not a valid value",
+            "prisonNumber": "A1234BC"
+          }"""
+        )
+        .exchange()
+        .expectStatus().isBadRequest
+        .expectBody().jsonPath("$.errorCode.code").isEqualTo(MalformedRequest.code)
+    }
+  }
+}
