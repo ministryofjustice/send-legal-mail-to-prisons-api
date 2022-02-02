24f3cbe6a648f6088d23bd76613a3db63c9fbe65
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-02-02 08:47:47 +0000

    SLM-62: Added happy path tests (#64)
    SLM-62: Added happy path tests

---
+++ src/testIntegration/resources/createContacts.sql
@@ -0,0 +1,13 @@
+INSERT INTO contacts (owner, name, prison_code, dob, prison_number, created, updated) values
+  ('some.user@company.com.cjsm.net', 'John Smith', 'BXI', null, 'A1234BC', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('some.user@company.com.cjsm.net', 'Johnathon Evans', 'SKI', null, 'S1243KO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('some.user@company.com.cjsm.net', 'Gage Hewitt', 'BXI', null, 'A9988BC', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('some.user@company.com.cjsm.net', 'Malcolm Johnston', 'ASI', '2000-08-13', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('some.user@company.com.cjsm.net', 'Henry Kelly', 'ASI', '1978-02-27', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('dave@aardvark.com.cjsm.net', 'Marty McFly', 'LEI', '1968-04-23', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('dave@aardvark.com.cjsm.net', 'John Matthews', 'LEI', '1968-04-23', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('john@aardvark.com.cjsm.net', 'Sally Edwards', 'ACI', null, 'G5429SE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('some.user@company.com.cjsm.net', 'Bart Johnson', 'ASI', '1964-06-19', null, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('some.user@company.com.cjsm.net', 'Johannes Locke', 'BFI', null, 'P4328DS', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+;
+
