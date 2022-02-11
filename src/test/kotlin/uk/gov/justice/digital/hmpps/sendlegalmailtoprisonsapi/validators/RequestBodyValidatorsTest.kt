848546d60f6b892fbc23911f848b46ccbef77bc2
Nathan Russell <nathan.russell@digital.justice.gov.uk>
2022-02-11 10:18:12 +0000

    SLM-122: Validator tests, plus tx boundary and tests to prove rollback

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/validators/RequestBodyValidatorsTest.kt
@@ -0,0 +1,91 @@
+package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.validators
+
+import org.assertj.core.api.Assertions.assertThatCode
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.barcode.CreateBarcodeRequest
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.config.ValidationException
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.contact.CreateContactRequest
+import java.time.LocalDate
+
+class RequestBodyValidatorsTest {
+
+  @Nested
+  inner class CreateContactRequestValidators {
+    @Test
+    fun `should validate given request with dob`() {
+      val createContactRequest = CreateContactRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", dob = LocalDate.now())
+
+      assertThatCode { validateRequestHasDobOrPrisonNumber(createContactRequest) }
+        .doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `should validate given request with prison number`() {
+      val createContactRequest = CreateContactRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", prisonNumber = "A1234BC")
+
+      assertThatCode { validateRequestHasDobOrPrisonNumber(createContactRequest) }
+        .doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `should validate given request with both dob and prison number`() {
+      val createContactRequest = CreateContactRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", dob = LocalDate.now(), prisonNumber = "A1234BC")
+
+      assertThatCode { validateRequestHasDobOrPrisonNumber(createContactRequest) }
+        .doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `should throw when validating given request without both dob and prison number`() {
+      val createContactRequest = CreateContactRequest(prisonerName = "Fred Bloggs", prisonId = "BXI")
+
+      assertThatExceptionOfType(ValidationException::class.java)
+        .isThrownBy { validateRequestHasDobOrPrisonNumber(createContactRequest) }
+    }
+  }
+
+  @Nested
+  inner class CreateBarcodeRequestValidators {
+    @Test
+    fun `should validate given null request`() {
+      val createBarcodeRequest: CreateBarcodeRequest? = null
+
+      assertThatCode { validateRequestHasDobOrPrisonNumber(createBarcodeRequest) }
+        .doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `should validate given request with dob`() {
+      val createBarcodeRequest = CreateBarcodeRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", dob = LocalDate.now())
+
+      assertThatCode { validateRequestHasDobOrPrisonNumber(createBarcodeRequest) }
+        .doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `should validate given request with prison number`() {
+      val createBarcodeRequest = CreateBarcodeRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", prisonNumber = "A1234BC")
+
+      assertThatCode { validateRequestHasDobOrPrisonNumber(createBarcodeRequest) }
+        .doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `should validate given request with both dob and prison number`() {
+      val createBarcodeRequest = CreateBarcodeRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", dob = LocalDate.now(), prisonNumber = "A1234BC")
+
+      assertThatCode { validateRequestHasDobOrPrisonNumber(createBarcodeRequest) }
+        .doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `should throw when validating given request without both dob and prison number`() {
+      val createBarcodeRequest = CreateBarcodeRequest(prisonerName = "Fred Bloggs", prisonId = "BXI")
+
+      assertThatExceptionOfType(ValidationException::class.java)
+        .isThrownBy { validateRequestHasDobOrPrisonNumber(createBarcodeRequest) }
+    }
+  }
+}
---
+++ src/testIntegration/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/barcode/BarcodeServiceTest.kt
@@ -0,0 +1,72 @@
+package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.barcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doThrow
+import org.mockito.kotlin.given
+import org.springframework.dao.CannotAcquireLockException
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.IntegrationTest
+
+class BarcodeServiceTest : IntegrationTest() {
+
+  @Nested
+  inner class CreateBarcode {
+    @Test
+    fun `should create barcode, barcode event, and barcode recipient`() {
+      val createBarcodeRequest = CreateBarcodeRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", prisonNumber = "A1234BC")
+      given { barcodeGeneratorService.generateBarcode() }.willReturn("SOME_CODE")
+
+      barcodeService.createBarcode("a-user@cjsm.net", createBarcodeRequest)
+
+      val barcode = barcodeRepository.getById("SOME_CODE")
+      assertThat(barcode).isNotNull
+      assertThat(barcodeEventRepository.findByBarcodeAndStatusCreated(barcode)).isNotNull
+      assertThat(barcodeRecipientRepository.getByBarcode(barcode)).isNotNull
+    }
+
+    @Test
+    fun `should rollback given creation of barcode record fails`() {
+      val createBarcodeRequest = CreateBarcodeRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", prisonNumber = "A1234BC")
+      given { barcodeGeneratorService.generateBarcode() }.willReturn("SOME_CODE")
+      doThrow(CannotAcquireLockException("some exception saving to the database")).`when`(barcodeRepository).save(any())
+
+      assertThatExceptionOfType(CannotAcquireLockException::class.java).isThrownBy {
+        barcodeService.createBarcode("a-user@cjsm.net", createBarcodeRequest)
+      }
+      assertThat(barcodeRepository.findById("SOME_CODE")).isNotPresent
+      assertThat(barcodeEventRepository.findAll()).isEmpty()
+      assertThat(barcodeRecipientRepository.findAll()).isEmpty()
+    }
+
+    @Test
+    fun `should rollback given creation of barcode event fails`() {
+      val createBarcodeRequest = CreateBarcodeRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", prisonNumber = "A1234BC")
+      given { barcodeGeneratorService.generateBarcode() }.willReturn("SOME_CODE")
+      doThrow(CannotAcquireLockException("some exception saving to the database")).`when`(barcodeEventRepository).save(any())
+
+      assertThatExceptionOfType(CannotAcquireLockException::class.java).isThrownBy {
+        barcodeService.createBarcode("a-user@cjsm.net", createBarcodeRequest)
+      }
+      assertThat(barcodeRepository.findById("SOME_CODE")).isNotPresent
+      assertThat(barcodeEventRepository.findAll()).isEmpty()
+      assertThat(barcodeRecipientRepository.findAll()).isEmpty()
+    }
+
+    @Test
+    fun `should rollback given creation of barcode recipient fails`() {
+      val createBarcodeRequest = CreateBarcodeRequest(prisonerName = "Fred Bloggs", prisonId = "BXI", prisonNumber = "A1234BC")
+      given { barcodeGeneratorService.generateBarcode() }.willReturn("SOME_CODE")
+      doThrow(CannotAcquireLockException("some exception saving to the database")).`when`(barcodeRecipientRepository).save(any())
+
+      assertThatExceptionOfType(CannotAcquireLockException::class.java).isThrownBy {
+        barcodeService.createBarcode("a-user@cjsm.net", createBarcodeRequest)
+      }
+      assertThat(barcodeRepository.findById("SOME_CODE")).isNotPresent
+      assertThat(barcodeEventRepository.findAll()).isEmpty()
+      assertThat(barcodeRecipientRepository.findAll()).isEmpty()
+    }
+  }
+}
