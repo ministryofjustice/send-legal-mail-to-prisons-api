0f39a350473daab86bb66fd2da4c6cf9106d807c
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-03-21 17:00:17 +0000

    SLM-182: Log JVM heap stats at application startup (#107)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/config/LogMemoryConfigurationAtStartupEvent.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.config
+
+import mu.KotlinLogging
+import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.stereotype.Component
+
+private val log = KotlinLogging.logger {}
+
+@Component
+class LogMemoryConfigurationAtStartupEvent : ApplicationListener<ContextRefreshedEvent> {
+
+  override fun onApplicationEvent(event: ContextRefreshedEvent) {
+    with(Runtime.getRuntime()) {
+      log.info { "heapSize: ${totalMemory().renderAsMb()}, heapMaxSize: ${maxMemory().renderAsMb()}, heapFreeSize: ${freeMemory().renderAsMb()}" }
+    }
+  }
+
+  private fun Long.renderAsMb(): String {
+    return "${this / 1024 / 1024}MB"
+  }
+}
