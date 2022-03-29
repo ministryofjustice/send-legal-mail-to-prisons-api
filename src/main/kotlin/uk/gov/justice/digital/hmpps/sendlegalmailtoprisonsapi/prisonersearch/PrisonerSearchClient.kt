b707a7d02139c8b5bde100f28c5662973e58b47e
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-03-29 14:28:21 +0100

    SLM-208: Log results from match-prisoners (#132)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/prisonersearch/PrisonerSearchClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/prisonersearch/PrisonerSearchClient.kt
@@ -1,11 +1,17 @@
 package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.prisonersearch
 
+import mu.KotlinLogging
+import org.springframework.http.HttpStatus.FORBIDDEN
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.prisonersearch.model.PagePrisoner
 import uk.gov.justice.digital.hmpps.prisonersearch.model.PrisonerMatches
+
+private val log = KotlinLogging.logger {}
 
 @Component
 class PrisonerSearchClient(private val prisonerSearchWebClient: WebClient) {
@@ -14,8 +20,39 @@ class PrisonerSearchClient(private val prisonerSearchWebClient: WebClient) {
       .post()
       .uri("/match-prisoners")
       .accept(MediaType.APPLICATION_JSON)
-      .bodyValue(prisonerSearchRequest.toRequestBody())
+      .bodyValue(prisonerSearchRequest.toMatchPrisonersRequestBody())
       .retrieve()
-      .bodyToMono()
+      .bodyToMono<PrisonerMatches>()
+      .onErrorResume { webClientErrorHandler(it) }
+    // In theory this is a possible way to submit the request on a different thread ... but it can't get the auth token because
+    // `userContext` is on the main request thread, so [WebClientConfiguration#addAuthHeaderFilterFunction] results in a NPE
+    // Needs some thought if we want to do this .... (other concurrency approaches may suffer the same fate if they use a different thread)
+    // .subscribeOn(Schedulers.parallel())
   }
+
+  fun globalSearch(prisonerSearchRequest: PrisonerSearchRequest): Mono<PagePrisoner> {
+    return prisonerSearchWebClient
+      .post()
+      .uri("/global-search")
+      .accept(MediaType.APPLICATION_JSON)
+      .bodyValue(prisonerSearchRequest.toGlobalSearchRequestBody())
+      .retrieve()
+      .bodyToMono<PagePrisoner>()
+      .onErrorResume { webClientErrorHandler(it) }
+  }
+
+  private fun <API_RESPONSE_BODY_TYPE> webClientErrorHandler(exception: Throwable): Mono<API_RESPONSE_BODY_TYPE> =
+    with(exception) {
+      if (this is WebClientResponseException) {
+        val uriPath = request?.uri?.path
+        if (statusCode == FORBIDDEN) {
+          log.info { "Client token does not have correct role to call prisoner-offender-search $uriPath" }
+        } else {
+          log.error { "Failed to call prisoner-offender-search $uriPath [statusCode: $statusCode, body: ${this.responseBodyAsString}]" }
+        }
+      } else {
+        log.error("Failed to call prisoner-offender-search", exception)
+      }
+    }
+      .let { Mono.empty() }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/prisonersearch/PrisonerSearchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/prisonersearch/PrisonerSearchService.kt
@@ -1,39 +1,64 @@
 package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.prisonersearch
 
 import mu.KotlinLogging
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.web.reactive.function.client.WebClientResponseException
-import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.prisonersearch.model.Prisoner
+import uk.gov.justice.digital.hmpps.prisonersearch.model.PrisonerMatch
+import uk.gov.justice.digital.hmpps.prisonersearch.model.PrisonerMatches
+import uk.gov.justice.digital.hmpps.prisonersearch.model.PrisonerMatches.MatchedBy.aLLSUPPLIED
+import uk.gov.justice.digital.hmpps.prisonersearch.model.PrisonerMatches.MatchedBy.aLLSUPPLIEDALIAS
 import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.barcode.BarcodeRecipient
+import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.security.UserContext
 
 private val log = KotlinLogging.logger {}
 
 @Service
-class PrisonerSearchService(private val prisonerSearchClient: PrisonerSearchClient) {
+class PrisonerSearchService(private val prisonerSearchClient: PrisonerSearchClient, private val userContext: UserContext) {
   fun lookupPrisoner(barcodeRecipient: BarcodeRecipient) {
     val prisonerSearchRequest = PrisonerSearchRequest(barcodeRecipient)
     prisonerSearchClient.matchPrisoners(prisonerSearchRequest)
-      .onErrorResume {
-        with(it) {
-          if (this is WebClientResponseException) {
-            if (statusCode == HttpStatus.FORBIDDEN) {
-              log.info { "Client token does not have correct role to call prisoner-search" }
-            } else {
-              log.error { "Failed to call prisoner-search - statusCode $statusCode" }
-            }
-          } else {
-            log.error("Failed to call prisoner-search", it)
-          }
-        }
-          .let { Mono.empty() }
-      }
-      // In theory this is a possible way to submit the request on a different thread ... but it can't get the auth token because
-      // `userContext` is on the main request thread, so [WebClientConfiguration#addAuthHeaderFilterFunction] results in a NPE
-      // Needs some thought if we want to do this .... (other concurrency approaches may suffer the same fate if they use a different thread)
-      // .subscribeOn(Schedulers.parallel())
-      .subscribe {
-        log.info { "Found ${it.matches.size} potential prisoner matches" }
-      }
+      .subscribe { processPrisonerMatches(prisonerSearchRequest, it) }
   }
+
+  private fun processPrisonerMatches(prisonerSearchRequest: PrisonerSearchRequest, prisonerMatches: PrisonerMatches) {
+    with(prisonerMatches) {
+
+      if (matches.isEmpty()) {
+        log.info { "/match-prisoners returned 0 potential prisoner matches" }
+        return
+      }
+
+      var bestMatch: Prisoner? = null
+      log.info { "/match-prisoners returned ${matches.size} potential prisoner matches by ${matchedBy.value}" }
+      if (matches.size == 1) {
+        bestMatch = matches[0].prisoner
+      } else if (matches.size > 1) {
+        // Find what we think is the best match
+        if (this.matchedBy == aLLSUPPLIED || this.matchedBy == aLLSUPPLIEDALIAS) {
+          // eg: we searched for a name and DOB, and /match-prisoners found n records that matched all criteria
+          bestMatch = matches.bestMatchBasedOnCurrentPrisonId()
+        } else {
+          // eg: we searched for a name and DOB, and /match-prisoners found n records that matched some of the criteria
+          // Our logic to find the best match will likely have to do more than match on prisonId, but this should do for now
+          bestMatch = matches.bestMatchBasedOnCurrentPrisonId()
+        }
+      }
+
+      if (bestMatch != null) {
+        log.info { "Best matched prisoner: prisonNumber matched: ${bestMatch.prisonerNumber == prisonerSearchRequest.prisonNumber}" }
+        log.info { "Best matched prisoner: Forwarding not required (prisonId matched): ${bestMatch.prisonId == userContext.caseload}" }
+        log.info { "Best matched prisoner: Has cell location: ${!bestMatch.cellLocation.isNullOrBlank()}" }
+      } else {
+        log.info { "/match-prisoners returned ${matches.size} potential matches but we could not filter it down to a best match" }
+      }
+    }
+  }
+
+  private fun List<PrisonerMatch>.bestMatchBasedOnCurrentPrisonId(): Prisoner? =
+    this.filter { prisonerMatch -> prisonerMatch.prisoner.prisonId == userContext.caseload }
+      .also {
+        log.info { "Filtered ${this.size} potential matches down to ${it.size} based on prisonId" }
+      }
+      .takeIf { it.size == 1 }
+      ?.first()?.prisoner
 }
