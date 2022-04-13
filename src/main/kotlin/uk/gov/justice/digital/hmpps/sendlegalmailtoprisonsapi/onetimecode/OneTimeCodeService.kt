9742ee498b61e1af9b417bba08b1042f7c55ef19
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-04-13 17:03:44 +0100

    SLM-230: Delete OTC every time it is checked. Do not support retries (#145)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/onetimecode/OneTimeCodeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/onetimecode/OneTimeCodeService.kt
@@ -26,12 +26,25 @@ class OneTimeCodeService(
     if (code == smokeTestConfig.lsjSecret) {
       smokeTest()
     } else {
+      // Initial MVP implementation that does not support retries
+      oneTimeCodeRepository.findById(sessionId)
+        .orElseGet { throw ResourceNotFoundException("One Time Code not found") } // OTC for the specified session ID not found
+        .also { oneTimeCodeRepository.deleteById(sessionId) }
+        .takeIf { oneTimeCode -> oneTimeCode.code.equals(code, ignoreCase = true) }
+        ?.let { oneTimeCode -> jwtService.generateToken(oneTimeCode.email, findOrganisation(oneTimeCode.email)) }
+        ?: throw ResourceNotFoundException("One Time Code not found") // OTC for the session ID found, but not matching the specified code
+
+      /*
+      // Implementation that will be the basis of a 3-strikes-and-out retry solution, in that the OTC is not deleted immediately if the code does not match.
+      // However, because no count or state for 3-strikes-and-out is currently in place, codes do not get deleted, and can be repeatedly retried up until
+      // their 30 minute expiration. Likely to be addressed in SLM-233
       oneTimeCodeRepository.findById(sessionId)
         .orElseGet { throw ResourceNotFoundException("One Time Code not found") }
         .takeIf { oneTimeCode -> oneTimeCode.code.equals(code, ignoreCase = true) }
         ?.also { oneTimeCodeRepository.deleteById(sessionId) }
         ?.let { oneTimeCode -> jwtService.generateToken(oneTimeCode.email, findOrganisation(oneTimeCode.email)) }
         ?: throw ResourceNotFoundException("One Time Code not found") // TODO - this is where '3 strikes and out' will be handled when we get to that
+       */
     }
 
   private fun smokeTest(): String = jwtService.generateToken("smoke-test-lsj", "smoke-test-lsj-org")
