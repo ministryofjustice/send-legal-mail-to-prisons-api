4054619f8fdc5781fe17ccdcca02c1e3b8f6ed56
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2022-05-24 08:24:15 +0100

    SLM-252 New service to remove a supported prison (#157)

--- src/testIntegration/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/prisons/SupportedPrisonsResourceTest.kt
+++ src/testIntegration/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/prisons/SupportedPrisonsResourceTest.kt
@@ -112,6 +112,28 @@ class SupportedPrisonsResourceTest : IntegrationTest() {
         .isEqualTo(listOf("ABC", true, "SOME_USER"))
       assertThat(supportedPrison.updated).isCloseTo(Instant.now(), within(5, ChronoUnit.SECONDS))
     }
+    @Test
+    fun `created if the prison was already active`() {
+      supportedPrisonsRepository.save(
+        aSupportedPrison(code = "ABC", active = true, updated = Instant.now().minus(1, ChronoUnit.DAYS))
+      )
+      PrisonRegisterExtension.prisonRegisterApi.stubGetPrisonOk("ABC")
+
+      webTestClient.post()
+        .uri("/prisons/ABC")
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(user = "SOME_USER", roles = listOf("ROLE_SLM_ADMIN")))
+        .exchange()
+        .expectStatus().isCreated
+
+      val supportedPrison = supportedPrisonsRepository.getById("ABC")
+
+      assertThat(supportedPrison)
+        .extracting(SupportedPrison::code, SupportedPrison::active, SupportedPrison::updatedBy)
+        .isEqualTo(listOf("ABC", true, "SOME_USER"))
+      assertThat(supportedPrison.updated).isCloseTo(Instant.now(), within(5, ChronoUnit.SECONDS))
+    }
 
     @Test
     fun `created if the prison has been reactivated`() {
@@ -133,6 +155,78 @@ class SupportedPrisonsResourceTest : IntegrationTest() {
       assertThat(supportedPrison)
         .extracting(SupportedPrison::code, SupportedPrison::active, SupportedPrison::updatedBy)
         .isEqualTo(listOf("CDE", true, "SOME_USER"))
+      assertThat(supportedPrison.updated).isCloseTo(Instant.now(), within(5, ChronoUnit.SECONDS))
+    }
+  }
+
+  @Nested
+  inner class RemoveSupportedPrison {
+    @Test
+    fun `unauthorised without a valid token`() {
+      webTestClient.delete()
+        .uri("/prisons/some-prison")
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    @Test
+    fun `forbidden without a valid role`() {
+      webTestClient.delete()
+        .uri("/prisons/some-prison")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(user = "AUSER_GEN"))
+        .exchange()
+        .expectStatus().isForbidden
+        .expectBody().jsonPath("$.errorCode.code").isEqualTo(AuthenticationError.code)
+    }
+
+    @Test
+    fun `not found if prison not in our database`() {
+      webTestClient.delete()
+        .uri("/prisons/ABC")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(user = "AUSER_GEN", roles = listOf("ROLE_SLM_ADMIN")))
+        .exchange()
+        .expectStatus().isNotFound
+        .expectBody().jsonPath("$.errorCode.code").isEqualTo(NotFound.code)
+    }
+
+    @Test
+    fun `ok if the prison has been removed`() {
+      supportedPrisonsRepository.save(aSupportedPrison(code = "ABC", active = true, updated = Instant.now().minus(1, ChronoUnit.DAYS)))
+
+      webTestClient.delete()
+        .uri("/prisons/ABC")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(user = "SOME_USER", roles = listOf("ROLE_SLM_ADMIN")))
+        .exchange()
+        .expectStatus().isOk
+
+      val supportedPrison = supportedPrisonsRepository.getById("ABC")
+      assertThat(supportedPrison)
+        .extracting(SupportedPrison::code, SupportedPrison::active, SupportedPrison::updatedBy)
+        .isEqualTo(listOf("ABC", false, "SOME_USER"))
+      assertThat(supportedPrison.updated).isCloseTo(Instant.now(), within(5, ChronoUnit.SECONDS))
+    }
+
+    @Test
+    fun `ok if the prison was already inactive`() {
+      supportedPrisonsRepository.save(
+        aSupportedPrison(code = "CDE", active = false, updated = Instant.now().minus(1, ChronoUnit.DAYS))
+      )
+
+      webTestClient.delete()
+        .uri("/prisons/CDE")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(user = "SOME_USER", roles = listOf("ROLE_SLM_ADMIN")))
+        .exchange()
+        .expectStatus().isOk
+
+      val supportedPrison = supportedPrisonsRepository.getById("CDE")
+      assertThat(supportedPrison)
+        .extracting(SupportedPrison::code, SupportedPrison::active, SupportedPrison::updatedBy)
+        .isEqualTo(listOf("CDE", false, "SOME_USER"))
       assertThat(supportedPrison.updated).isCloseTo(Instant.now(), within(5, ChronoUnit.SECONDS))
     }
   }
