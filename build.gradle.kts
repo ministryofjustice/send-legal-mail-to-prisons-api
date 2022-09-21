8c4a7dc5e3637382c4adcda306fe83254b332d38
Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
2022-09-21 12:37:57 +0100

    Update hmpps.gradle-spring-boot version to latest - 4.5.1 to ensure security checks do not fail. (#178)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.1"
   id("org.unbroken-dome.test-sets") version "4.0.0"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
