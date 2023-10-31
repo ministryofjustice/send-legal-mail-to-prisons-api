a95d0e245d2ea7e2f54359936aba91fd62d80529
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-10-31 15:35:35 +0000

    SLM-298 temporary code update - to be reverted to existing subscribe call.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/prisonersearch/PrisonerSearchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/prisonersearch/PrisonerSearchService.kt
@@ -1,6 +1,10 @@
 package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.prisonersearch
 
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.prisonersearch.model.PagePrisoner
+import uk.gov.justice.digital.hmpps.prisonersearch.model.PrisonerMatch
+import uk.gov.justice.digital.hmpps.prisonersearch.model.PrisonerMatches
 import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.barcode.BarcodeRecipient
 import uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.security.UserContext
 
@@ -10,18 +14,29 @@ class PrisonerSearchService(
   private val prisonerSearchResultsProcessor: PrisonerSearchResultsProcessor,
   private val userContext: UserContext,
 ) {
+
   fun lookupPrisoner(barcodeRecipient: BarcodeRecipient): Boolean {
     val prisonerSearchRequest = PrisonerSearchRequest(barcodeRecipient)
     val caseload = userContext.caseload
     var forwardingNeeded = false
-    prisonerSearchClient.matchPrisoners(prisonerSearchRequest).subscribe {
-      prisonerSearchResultsProcessor.processSearchResults(it, prisonerSearchRequest, caseload)?.let { prisoner ->
-        forwardingNeeded = forwardingNeeded || (prisoner.prisonId != caseload)
+    // TODO - revert back later. added to check on dev env for the timebeing
+    prisonerSearchClient.matchPrisoners(prisonerSearchRequest).onErrorResume {
+      return@onErrorResume Mono.just(PrisonerMatches(listOf<PrisonerMatch>(), PrisonerMatches.MatchedBy.nOTHING))
+    }.block()?.let {
+      if (it.matches.isNotEmpty()) {
+        prisonerSearchResultsProcessor.processSearchResults(it, prisonerSearchRequest, caseload)?.let { prisoner ->
+          forwardingNeeded = (prisoner.prisonId != caseload)
+        }
       }
     }
-    prisonerSearchClient.globalSearch(prisonerSearchRequest).subscribe {
-      prisonerSearchResultsProcessor.processSearchResults(it, prisonerSearchRequest, caseload)?.let { prisoner ->
-        forwardingNeeded = forwardingNeeded || (prisoner.prisonId != caseload)
+
+    prisonerSearchClient.globalSearch(prisonerSearchRequest).onErrorResume {
+      return@onErrorResume Mono.just(PagePrisoner(totalElements = 0))
+    }.block()?.let {
+      if (it.totalElements != null && it.totalElements > 0) {
+        prisonerSearchResultsProcessor.processSearchResults(it, prisonerSearchRequest, caseload)?.let { prisoner ->
+          forwardingNeeded = forwardingNeeded || (prisoner.prisonId != caseload)
+        }
       }
     }
 
