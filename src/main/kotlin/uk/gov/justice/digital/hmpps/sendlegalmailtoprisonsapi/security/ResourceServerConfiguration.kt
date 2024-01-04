ecf8b0006959c343d892e990143183239ff1ba4c
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-01-04 09:19:33 +0000

    SLM-334 - remove locallyCachedJwtDecoder.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/security/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sendlegalmailtoprisonsapi/security/ResourceServerConfiguration.kt
@@ -1,7 +1,5 @@
 package uk.gov.justice.digital.hmpps.sendlegalmailtoprisonsapi.security
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
@@ -14,8 +12,6 @@ import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
@@ -77,10 +73,4 @@ class ResourceServerConfiguration(private val barcodeUserDetailsService: UserDet
   fun userDetailsServiceWrapper(): UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> =
     UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken>()
       .apply { setUserDetailsService(barcodeUserDetailsService) }
-
-  @Bean
-  fun locallyCachedJwtDecoder(
-    @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") jwkSetUri: String,
-    cacheManager: CacheManager,
-  ): JwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).cache(cacheManager.getCache("jwks")).build()
 }
